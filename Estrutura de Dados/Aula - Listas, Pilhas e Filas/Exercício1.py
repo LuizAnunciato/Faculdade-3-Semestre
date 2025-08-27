cb1611f13404c8bab4c86e07426d340df3fab30b
Luiz Felipe Anunciato Nobre <mcluizspessoal@gmail.com>
2025-08-27 20:54:53 -0300

    Exercício1.py

---
+++ Estrutura de Dados/Aula - Listas, Pilhas e Filas/Exercício1.py
@@ -0,0 +1,32 @@
+#Exercício 1
+
+#Lista Vazia
+pilha = []
+
+print(f"Pilha vazia? {not pilha}")
+print()
+#Operação "Push"
+#Empilhar
+pilha.append(10)
+pilha.append(20)
+pilha.append(30)
+
+print(f"Pilha depois do Push: {pilha}")
+print()
+
+#Operação Top
+#Acessa o último elemento da lista
+topo = pilha[-1]
+print(f"Elemento no topo: {topo}")
+print()
+
+#Operação desempilhar
+desempilhar = pilha.pop()
+print(f"Pilha depois do Pop: {pilha}")
+print()
+
+#Verificar se a lista está vazia
+if not pilha:
+    print("A pilha está vazia")
+else:
+    print("A pilha não está vazia")
